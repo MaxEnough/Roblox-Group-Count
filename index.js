0af1505b1f63b765cac0262930cf30473c730626
Max <46148308+MaxEnough@users.noreply.github.com>
2021-02-03 09:33:43 -0700

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ let group = 4382797; //Group ID
 let goal = 100000; //Member Goal
 
 const webhook = new client.WebhookClient(
-  "718957476643430194630", //Webhook ID
-  "kfsgCxKmx8dk47yECOYEW9ttGUwER9o9dfs3344dDpVxBztbLdX1BOmSvmoBqlmAtv_4W" //Webhook Token
+  "id", //Webhook ID
+  "token" //Webhook Token
 );
 async function updateCount() {
   let response = await axios.get(
